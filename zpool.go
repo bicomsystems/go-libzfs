6b487f8211c20ebc7d707bfab0fe471b71aef612
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2023-07-12 00:31:10 +0100

    zfs-2.2-rc1 compat
    Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

--- zpool.go
+++ zpool.go
@@ -90,7 +90,7 @@ type PoolScanStat struct {
 	EndTime   uint64 // Scan end time
 	ToExamine uint64 // Total bytes to scan
 	Examined  uint64 // Total bytes scaned
-	ToProcess uint64 // Total bytes to processed
+	Skipped   uint64 // Total bytes skipped by scanner
 	Processed uint64 // Total bytes processed
 	Errors    uint64 // Scan errors
 	// Values not stored on disk
@@ -201,7 +201,7 @@ func poolGetConfig(name string, nv C.nvlist_ptr) (vdevs VDevTree, err error) {
 		vdevs.ScanStat.EndTime = uint64(ps.pss_end_time)
 		vdevs.ScanStat.ToExamine = uint64(ps.pss_to_examine)
 		vdevs.ScanStat.Examined = uint64(ps.pss_examined)
-		vdevs.ScanStat.ToProcess = uint64(ps.pss_to_process)
+		vdevs.ScanStat.Skipped = uint64(ps.pss_skipped)
 		vdevs.ScanStat.Processed = uint64(ps.pss_processed)
 		vdevs.ScanStat.Errors = uint64(ps.pss_errors)
 		vdevs.ScanStat.PassExam = uint64(ps.pss_pass_exam)
